3d201ab783937234ebcfec3571fc8b100ff589b6
MRitz <MRitz@ADC-NB10850.intern.cube.ch>
2018-10-04 14:10:16 +0200

    1

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,48 +1,71 @@
 package org.leanpoker.player;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
-import java.util.Map;
-import java.util.Random;
 
 public class Player {
     static final String VERSION = "Default Java folding player";
     
     public static int betRequest(JsonElement request) {
     	JsonElement ja = ((JsonObject)request).get("players");
+    	
+    	int round = ((JsonObject)request).get("round").getAsInt();
     	int max = 0;
+    	int active = 0;
 //    	
     	for (JsonElement jp : ja.getAsJsonArray()) {
 			JsonElement jname = ((JsonObject)jp).get("name");
 			if (jname.getAsString().contains("oker")) {
-				JsonElement jst = ((JsonObject)jp).get("stack");
+				//JsonElement jst = ((JsonObject)jp).get("stack");
 				//return jst.getAsInt();
+				//--hole_cards(jp);
 			 }
 			 
 			JsonElement jbet = ((JsonObject)jp).get("bet");
 			if (jbet.getAsInt() > max) {
 				max = jbet.getAsInt();
 			}
+			
+			JsonElement jactive = ((JsonObject)jp).get("status");
+			if (! "out".equals(jactive.getAsString())) {
+				active++;
+			}
 		}
-//    	
-        //return max;
-    	//System.out.println("->"+max);
-    	return max+1; 
+    	
+    	//community_cards(request);
+    	if (active <= 2)
+    		return max+1;
+    	return 0;
     }
 
     public static void showdown(JsonElement game) {
     }
     
-    String[] evalCards(JsonElement jWir) {
-    	String[] ret = new String[2];
+
+    static String[] hole_cards(JsonElement jWir) {
+    	String[] ret = new String[4];
     	JsonElement hc = ((JsonObject)jWir).get("hole_cards");
-    	ret[0] = hc.getAsJsonArray().get(0).getAsString();
-    	ret[1] = hc.getAsJsonArray().get(0).getAsString();
     	
-    	System.out.println(ret[0]);
-    	System.out.println(ret[1]);
+    	ret[0] = ((JsonObject)hc.getAsJsonArray().get(0)).get("rank").getAsString();
+    	ret[1] = ((JsonObject)hc.getAsJsonArray().get(0)).get("suit").getAsString();
+      	ret[2] = ((JsonObject)hc.getAsJsonArray().get(1)).get("rank").getAsString();
+    	ret[3] = ((JsonObject)hc.getAsJsonArray().get(1)).get("suit").getAsString();
+    	
+    	return ret;
+    }
+
+    
+    static String[] community_cards(JsonElement jo) {
+    	String[] ret = new String[6];
+    	JsonElement hc = ((JsonObject)jo).get("community_cards");
+    	
+    	ret[0] = ((JsonObject)hc.getAsJsonArray().get(0)).get("rank").getAsString();
+    	ret[1] = ((JsonObject)hc.getAsJsonArray().get(0)).get("suit").getAsString();
+      	ret[2] = ((JsonObject)hc.getAsJsonArray().get(1)).get("rank").getAsString();
+    	ret[3] = ((JsonObject)hc.getAsJsonArray().get(1)).get("suit").getAsString();
+      	ret[4] = ((JsonObject)hc.getAsJsonArray().get(2)).get("rank").getAsString();
+    	ret[5] = ((JsonObject)hc.getAsJsonArray().get(2)).get("suit").getAsString();
+    	
     	return ret;
     }
     
