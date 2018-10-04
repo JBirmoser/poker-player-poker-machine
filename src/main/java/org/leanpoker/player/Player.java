cc45d10ec86d1d9e74c198cab62da42cf1ed1572
MRitz <MRitz@ADC-NB10850.intern.cube.ch>
2018-10-04 15:10:42 +0200

    1

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -51,7 +51,7 @@ public class Player {
     	
     	
     	
-    	String[] community_cards = community_cards(request);
+//    	String[] community_cards = community_cards(request);
 //    	if(community_cards.length > 1 
 //    			&& containsSameRankWithCount(community_cards, hole_ranks, 3)) {
 //    		return max+1;
