001db82087ee79e7e3a7e8e59e3eb8d223996606
MRitz <MRitz@ADC-NB10850.intern.cube.ch>
2018-10-04 10:49:29 +0200

    2

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,15 +3,17 @@ package org.leanpoker.player;
 import com.google.gson.JsonElement;
 
 import java.util.Map;
+import java.util.Random;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
+    static Random r = new Random();
+    
     public static int betRequest(JsonElement request) {
     	
-    	
-        return 1;
+        return r.nextInt() % 10;
     }
 
     public static void showdown(JsonElement game) {
